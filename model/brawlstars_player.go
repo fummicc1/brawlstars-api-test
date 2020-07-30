afe3025b38deb46d4233b2072e068ee958532ada
fummicc1 <fumiya.tennis1@gmail.com>
2020-07-30 21:55:51 +0900

    fix type

--- model/brawlstars_player.go
+++ model/brawlstars_player.go
@@ -1,16 +1,16 @@
 package model
 
 type BrawlStarsPlayer struct {
-	Tag                                  string                `json:"tag"`
-	Name                                 string                `json:"name"`
-	Icon                                 *BrawlStarsPlayerIcon `json:"id"`
-	Trophies                             int                   `json:"trophies"`
-	HighestTrophies                      int                   `json:"highest_trophies"`
-	PowerPlayPoints                      int                   `json:"powerPlayPoints"`
-	ExpLevel                             int                   `json:"expLevel"`
-	ExpPoints                            int                   `json:"expPoints"`
-	IsQualifiedFromChampionshipChallenge bool                  `json:"isQualifiedFromChampionshipChallenge"`
-	Brawlers                             []Brawler             `json: "brawlers"`
+	Tag                                  string               `json:"tag"`
+	Name                                 string               `json:"name"`
+	Icon                                 BrawlStarsPlayerIcon `json:"id"`
+	Trophies                             int                  `json:"trophies"`
+	HighestTrophies                      int                  `json:"highest_trophies"`
+	PowerPlayPoints                      int                  `json:"powerPlayPoints"`
+	ExpLevel                             int                  `json:"expLevel"`
+	ExpPoints                            int                  `json:"expPoints"`
+	IsQualifiedFromChampionshipChallenge bool                 `json:"isQualifiedFromChampionshipChallenge"`
+	Brawlers                             []Brawler            `json: "brawlers"`
 }
 
 type BrawlStarsPlayerIcon struct {
